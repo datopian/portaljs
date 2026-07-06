19e15479eaabfb8ab932a42dd9e47c10bce38692
Anuar Ustayev <anuar.ustayev@gmail.com>
2026-07-06 16:02:58 +0500

    WIP: checkpoint (auto)

--- packages/ckan/.eslintrc.cjs
+++ packages/ckan/.eslintrc.cjs
@@ -1,9 +1,10 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2020: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
